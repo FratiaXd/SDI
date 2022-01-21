69d711598222f06bba6575e3ed5097307e7f189c
FratiaXd <welcomcherry@gmail.com>
2022-01-21 07:08:34 -0800

    Restart

---
+++ sdi_app/login.cpp
@@ -0,0 +1,15 @@
+#include "headers/login.h"
+#include "./ui_login.h"
+
+login::login(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::login)
+{
+    ui->setupUi(this);
+}
+
+login::~login()
+{
+    delete ui;
+}
+
