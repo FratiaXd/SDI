06e1ab486580073bd621570df0cf48739c29e503
Umar <121aliumar@gmail.com>
2022-04-22 20:45:57 +0100

    i hate this coursework so much

--- sdi_app/src/cargo_owner.cpp
+++ sdi_app/src/cargo_owner.cpp
@@ -3,6 +3,11 @@
 //
 #include "headers/cargo_owner.h"
 #include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -25,10 +30,59 @@ double CargoOwner::calculate_shipping(float width, float height, float length, f
     // Weight measured in kilograms
     // Distance measured in kilometers
     //
-    double longSource = 0, latSource = 0, longDest = 0, latDest = 0;
+    double longSource = 0;
+    double latSource = 0;
+    double longDest = 0;
+    double latDest = 0;
+    if (src == "Nottingham") {
+        longSource = 52.940;
+        latSource =  1.1550;
+    } else if (src == "Leeds") {
+        longSource = 53.8008;
+        latSource =  1.5491;
+    } else if (src == "Liverpool") {
+        longSource = 53.4084;
+        latSource =  2.9916;
+    } else if (src == "London") {
+        longSource = 51.5072;
+        latSource =  0.1276;
+    } else if (src == "Manchester") {
+        longSource = 53.4808;
+        latSource =  2.2426;
+    } else if (src == "Birmingham") {
+        longSource = 52.4862;
+        latSource =  1.8904;
+    } else if (src == "Edinburgh") {
+        longSource = 55.9533;
+        latSource =  3.1883;
+    }
+
+    if (addr == "Nottingham") {
+        longDest = 52.940;
+        latDest =  1.1550;
+    } else if (addr == "Leeds") {
+        longDest = 53.8008;
+        latDest =  1.5491;
+    } else if (addr == "Liverpool") {
+        longDest = 53.4084;
+        latDest =  2.9916;
+    } else if (addr == "London") {
+        longDest = 51.5072;
+        latDest =  0.1276;
+    } else if (addr == "Manchester") {
+        longDest = 53.4808;
+        latDest =  2.2426;
+    } else if (addr == "Birmingham") {
+        longDest = 52.4862;
+        latDest =  1.8904;
+    } else if (addr == "Edinburgh") {
+        longDest = 55.9533;
+        latDest =  3.1883;
+    }
+
     double dLat = (latSource - latDest) *
                   M_PI / 180.0;
-    double dLon = (longSource - longSource) *
+    double dLon = (longSource - longDest) *
                   M_PI / 180.0;
 
     // convert to radians
@@ -44,6 +98,8 @@ double CargoOwner::calculate_shipping(float width, float height, float length, f
     double distanceKilometers = rad * c;
 
     double finalShippingCost = ((distanceKilometers/2) * (weight) * (width * length * height));
+    finalShippingCost = finalShippingCost / 100;
+    finalShippingCost = std::ceil(finalShippingCost * 100.0) / 100.0;
     return finalShippingCost;
 }
 
